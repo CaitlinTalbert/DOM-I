f9bc4aa4d59090ba3093394374acd056cfa7d669
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-11-29 15:17:01 -0600

    wrote navs-still nothing showing up

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "nav-item-5": "About",
     "nav-item-6": "Contact",
   },
+
   "cta": {
     "h1": "DOM Is Awesome",
     "button": "Get Started",
   },
+
   "main-content": {
     "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
@@ -33,10 +35,56 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "copyright": "Copyright Great Idea! 2021",
   },
   "images": {
-    "logo-img": "http://localhost:9000/img/logo.png",
-    "cta-img": "http://localhost:9000/img/cta.png",
-    "accent-img": "http://localhost:9000/img/accent.png",
+    "logo-img": "mocks/img/logo.png",
+    "cta-img": "mocks/img/cta.png",
+    "accent-img": "mocks/img/accent.png",
   },
 };
 
 console.log('project wired!')
+
+    const navbar = document.querySelector('nav'); 
+    navbar.style.fontStyle = "italic"; 
+    const nav1 = document.querySelector('nav a:nth-of-type(1)'); 
+    const nav2 = document.querySelector('nav-item-2'); 
+    const nav3 = document.querySelector('nav-item-3'); 
+    const nav4 = document.querySelector('nav-item-4'); 
+    const nav5 = document.querySelector('nav-item-5'); 
+    const nav6 = document.querySelector('nav-item-6'); 
+
+    nav1.textContent = siteContent["nav"]["nav-item-1"]; 
+    nav2.textContent = siteContent["nav"]["nav-item-2"]; 
+    nav3.textContent = siteContent["nav"]["nav-item-3"]; 
+    nav4.textContent = siteContent["nav"]["nav-item-4"]; 
+    nav5.textContent = siteContent["nav"]["nav-item-5"]; 
+    nav6.textContent = siteContent["nav"]["nav-item-6"]; 
+
+
+    const companyLogo = document.querySelector('#logo-img'); 
+    companyLogo = siteContent["images"]["logo-img"]; 
+
+
+    const ctaText = document.querySelector('h1'); 
+    ctaText.textContent = sitecontent['cta']['h1']; 
+
+    const buttonText = document.querySelector('button'); 
+    buttonText.innerText = siteContent["cta"]["button"]; 
+
+
+
+    /*const siteContent = {
+      "contact": {
+      "contact-h4": "Contact",
+      "address": "123 Way 456 Street Somewhere, USA",
+      "phone": "1 (888) 888-8888",
+      "email": "sales@greatidea.io",
+      }
+    }; 
+
+    const contactH4 = document.querySelector('.contactH4'); 
+    const address = document.querySelector(".address"); 
+    const phone = document.querySelector(".phone"); 
+    const email = document.querySelector(".email"); 
+
+    contactH4.textContent = data["contact"]["contactH4"]; 
+    address.textContent = data["contact"]["address"]; */
